d71708639109653fc1bf18440bf6e661cc55bf52
ir2sora <ir2sora@gmail.com>
2015-11-04 22:11:13 +0300

    add frontend skeleton

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -14,9 +14,33 @@ phonecatApp.config(['$routeProvider',
         templateUrl: 'partials/index.html',
         controller: 'IndexCtrl'
       }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
+      when('/candy-list', {
+        templateUrl: 'partials/candy-list.html',
+        controller: 'CandyListCtrl'
+      }).
+      when('/candy-add', {
+        templateUrl: 'partials/candy-add.html',
+        controller: 'CandyAddCtrl'
+      }).
+      when('/candy/:candyId/edit', {
+        templateUrl: 'partials/candy-edit.html',
+        controller: 'CandyEditCtrl'
+      }).
+      when('/present-add', {
+        templateUrl: 'partials/present-add.html',
+        controller: 'PresentAddCtrl'
+      }).
+      when('/present/:presentId/add', {
+        templateUrl: 'partials/present-add.html',
+        controller: 'PresentAddCtrl'
+      }).
+      when('/present-list', {
+        templateUrl: 'partials/present-list.html',
+        controller: 'PresentListCtrl'
+      }).
+      when('/present/:presentId', {
+        templateUrl: 'partials/present-show.html',
+        controller: 'PresentShowCtrl'
       }).
       otherwise({
         redirectTo: '/index'
--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -5,4 +5,47 @@
 var presentControllers = angular.module('presentControllers', []);
 
 presentControllers.controller('IndexCtrl', function() {
-  });
+});
+
+presentControllers.controller('CandyListCtrl', ["$scope", "$location", "$anchorScroll", function($scope, $location, $anchorScroll) {
+   $scope.$on('$viewContentLoaded', function(){
+      $("div#sticky").sticky({ topSpacing: 0 });
+    });
+
+   $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+   }
+}]);
+
+presentControllers.controller('CandyAddCtrl', ["$scope", function($scope) {
+}]);
+
+presentControllers.controller('CandyEditCtrl', ["$scope", function($scope) {
+}]);
+
+presentControllers.controller('PresentAddCtrl', ["$scope", "$location", "$anchorScroll", function($scope, $location, $anchorScroll) {
+   $scope.$on('$viewContentLoaded', function(){
+      $("div#sticky").sticky({ topSpacing: 0 });
+    });
+
+   $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+   }
+}]);
+
+presentControllers.controller('PresentListCtrl', ["$scope", "$location", "$anchorScroll", function($scope, $location, $anchorScroll) {
+   $scope.$on('$viewContentLoaded', function(){
+      $("div#sticky").sticky({ topSpacing: 0 });
+    });
+
+   $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+   }
+}]);
+
+presentControllers.controller('PresentShowCtrl', ["$scope", function($scope) {
+}]);
+
