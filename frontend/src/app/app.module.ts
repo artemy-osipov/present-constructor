6b5aa777a6bb108a23bc21d204be117dbb9461b4
ir2sora <ir2sora@gmail.com>
2017-08-02 20:20:31 +0300

    add header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
