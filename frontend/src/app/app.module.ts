2ae4f3f0cc2e9513c2b96f3cfe27b3ab50827bf5
ir2sora <ir2sora@gmail.com>
2017-08-07 21:48:11 +0300

    template for candy list

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
+import { CandyListComponent } from './candy-list/candy-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    HeaderComponent
+    HeaderComponent,
+    CandyListComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/candy-list/candy-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candy-list',
+  templateUrl: './candy-list.component.html',
+  styleUrls: ['./candy-list.component.css']
+})
+export class CandyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
