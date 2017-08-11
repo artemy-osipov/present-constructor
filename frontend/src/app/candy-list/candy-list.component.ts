70afbfb1c8e1e25f5a9f1710c9833aa512da33a9
ir2sora <ir2sora@gmail.com>
2017-08-11 23:22:49 +0300

    improve frontend template

--- frontend/src/app/candy-list/candy-list.component.ts
+++ frontend/src/app/candy-list/candy-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Candy } from '../shared/candy.model';
 
 @Component({
   selector: 'app-candy-list',
   templateUrl: './candy-list.component.html',
   styleUrls: ['./candy-list.component.css']
 })
-export class CandyListComponent implements OnInit {
+export class CandyListComponent {
+  candies: Candy[];
 
-  constructor() { }
+  constructor() {
+    const candies: Candy[] = [];
 
-  ngOnInit() {
+    for (let i = 1; i < 20; i++) {
+      candies.push(new Candy('f2e35882-5168-4ca5-8925-dd5f137857eb', 'Название ' + i, 'Производитель ' + i, i, i));
+    }
+
+    this.candies = candies;
   }
-
 }
---
+++ frontend/src/app/shared/candy.model.ts
@@ -0,0 +1,9 @@
+export class Candy {
+  constructor(
+    public id: string,
+    public name: string,
+    public firm: string,
+    public price: number,
+    public order: number) {
+    }
+}
