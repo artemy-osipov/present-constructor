5f4e4c6a04545a694add64c9b94d5029a781c932
artemy-osipov <osipov.artemy@gmail.com>
2017-12-23 17:01:27 +0300

    refactor config

--- backend/src/main/java/ru/home/shop/config/JacksonConfig.java
+++ backend/src/main/java/ru/home/shop/config/JacksonConfig.java
@@ -1,17 +1,10 @@
 package ru.home.shop.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import org.springframework.boot.jackson.JsonComponent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
@@ -19,26 +12,15 @@ import java.io.IOException;
 @Configuration
 public class JacksonConfig {
 
-    @Bean
-    @Primary
-    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-        ObjectMapper mapper = builder.createXmlMapper(false).build();
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.registerModule(new JavaTimeModule());
-
-        return mapper;
-    }
-
     @JsonComponent
-    public static class TrimDeserializer extends StdScalarDeserializer<String> {
+    public static class TrimDeserializer extends StdDeserializer<String> {
 
         public TrimDeserializer() {
             super(String.class);
         }
 
         @Override
-        public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        public String deserialize(JsonParser jsonParser, DeserializationContext context) throws IOException {
             String value = jsonParser.getValueAsString();
 
             return StringUtils.hasText(value) ? StringUtils.trimWhitespace(value) : null;
