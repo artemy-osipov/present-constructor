26902b081160938633dfbe3617326793f5541941
artemy-osipov <osipov.artemy@gmail.com>
2018-02-03 14:11:41 +0300

    upgrade spring boot

--- backend/src/main/java/ru/home/shop/PresentsApplication.java
+++ backend/src/main/java/ru/home/shop/PresentsApplication.java
@@ -2,7 +2,7 @@ package ru.home.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PresentsApplication {
