60ba2bf22b9ba059eee06d9ea9cd29964b8c0427
artemy-osipov <osipov.artemy@gmail.com>
2018-11-12 18:39:46 +0300

    refactor: explicit ng-bootstrap modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,21 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MobxAngularModule } from 'mobx-angular';
-
-import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-  faAngleDown,
-  faAngleUp,
-  faCopy,
-  faEye,
-  faFile,
-  faPencilAlt,
-  faPlus,
-  faTimes,
-  faTrash
-} from '@fortawesome/free-solid-svg-icons';
+  NgbAlertModule,
+  NgbModalModule,
+  NgbTooltipModule
+} from '@ng-bootstrap/ng-bootstrap';
+import { MobxAngularModule } from 'mobx-angular';
 
 import { AboutComponent } from 'app/about/about.component';
 import { AppRoutingModule } from 'app/app-routing.module';
@@ -37,6 +28,19 @@ import { CandyStore } from 'app/shared/services/candy.store';
 import { PresentApi } from 'app/shared/services/present.api.service';
 import { PresentStore } from 'app/shared/services/present.store';
 import { ValidationModule } from 'app/shared/validation';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faAngleDown,
+  faAngleUp,
+  faCopy,
+  faEye,
+  faFile,
+  faPencilAlt,
+  faPlus,
+  faTimes,
+  faTrash
+} from '@fortawesome/free-solid-svg-icons';
 
 library.add(
   faAngleDown,
@@ -71,9 +75,11 @@ library.add(
     FontAwesomeModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
     MobxAngularModule,
-    NgbModule,
+    NgbAlertModule,
+    NgbModalModule,
+    NgbTooltipModule,
+    ReactiveFormsModule,
     SecurityModule,
     ValidationModule
   ],
