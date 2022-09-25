999a1c09250f419bca97aeb2d91619979c9299c4
artemy-osipov <osipov.artemy@gmail.com>
2022-09-25 17:45:53 +0300

    deps(backend): downgrade netty to 4.1.77

--- backend/src/main/kotlin/io/github/artemy/osipov/shop/Application.kt
+++ backend/src/main/kotlin/io/github/artemy/osipov/shop/Application.kt
@@ -28,6 +28,14 @@ import org.springframework.nativex.hint.TypeHint
 )
 @TypeHint(
     types = [
+        java.util.Collection::class,
+        java.time.LocalDateTime::class,
+        Array<java.time.Instant>::class,
+        Array<java.time.ZonedDateTime>::class
+    ]
+)
+@TypeHint(
+    types = [
         ReportPresent::class,
         ReportPresent.Item::class
     ],
